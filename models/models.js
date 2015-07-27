2541cab7be5aced1015658fde96161be24f33c8b
Edgar De La Cruz Esparza <edelacruz1212@gmail.com>
2015-07-26 22:54:14 -0500

    Autoload verifica si existe un quizId

--- models/models.js
+++ models/models.js
@@ -35,7 +35,7 @@ exports.Quiz = Quiz; // exportar tabla Quiz
 sequelize.sync().then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
-    if(count === 3) {   // la tabla se inicializa solo si está vacía
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
    Quiz.create({ pregunta: 'Capital de Italia',
                     respuesta: 'Roma'
                 });
