f5654a122603d9dab59491f3f65e262749e7c4f2
Edgar De La Cruz Esparza <edelacruz1212@gmail.com>
2015-08-09 19:30:02 -0500

    Moulo 9. Autenticacion y sesion

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+//controlador de sesiones
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +13,10 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+
+router.get('/login',  sessionController.new);  // formulario login
+router.post('/login', sessionController.create); // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Creditos' });
