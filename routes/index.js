dc934ff1eef67c7cc78fa591e5f6e637dbe7b634
Edgar De La Cruz Esparza <edelacruz1212@gmail.com>
2015-08-09 20:58:18 -0500

    Modulo 9, moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,8 @@ router.get('/', function(req, res) {
 });
 
 //Autoload de comandos con :quizId
-router.param('quizId', quizController.load);
+router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load); //autoload :commentId
 
 router.get('/login',  sessionController.new);  // formulario login
 router.post('/login', sessionController.create); // crear sesión
@@ -33,7 +34,10 @@ router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizCo
 router.put('/quizes/:quizId(\\d+)',	 sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',	 sessionController.loginRequired, quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+// Deberia ser PUT ya que se va ser una actualización
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
